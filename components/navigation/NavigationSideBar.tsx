b86706ddc0b468894ec383f46ef4ac7bc98ef136
Dev-mininos <amine.lbabda2@gmail.com>
2023-11-26 11:55:52 +0100

    sidebar-part1

--- components/navigation/NavigationSideBar.tsx
+++ components/navigation/NavigationSideBar.tsx
@@ -3,6 +3,10 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavigationAction from "./NavigationAction";
 import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./NavigationItem";
+import { ModeToggle } from "../mode-toogle";
+import { UserButton } from "@clerk/nextjs";
 
 const NavigationSideBar = async () => {
   const profile = await cuurentProfile();
@@ -23,6 +27,28 @@ const NavigationSideBar = async () => {
     <div className="flex h-full w-full flex-col items-center space-y-4 py-3 text-primary dark:bg-[#1E1F22]">
       <NavigationAction />
       <Separator className="mx-auto h-[2px] w-10 rounded-md bg-zinc-300 dark:bg-zinc-700" />
+      <ScrollArea className="w-full flex-1">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="mt-auto flex flex-col items-center gap-y-4 pb-3">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
