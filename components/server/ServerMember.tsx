ddbcd8e054e94bdafc221f15da2937594bbecfdd
Dev-mininos <amine.lbabda2@gmail.com>
2023-12-13 16:39:26 +0100

    fixing errors:DONE

--- components/server/ServerMember.tsx
+++ components/server/ServerMember.tsx
@@ -42,7 +42,7 @@ export const ServerMember: React.FC<ServerMemberProps> = ({
       <p
         className={cn(
           "text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300",
-          params.memberId === member.id &&
+          params?.memberId === member.id &&
             "text-primary dark:text-zinc-200 dark:group-hover:text-white ",
         )}
       >
